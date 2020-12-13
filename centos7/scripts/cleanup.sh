b60ce47c076aad7bb6a3bddd5ccf4eba18732a15
Gabor Foris <gabor_foris@epam.com>
2020-12-13 21:35:16 +0100

    removing /root/anaconda-ks.cfg

--- centos7/scripts/cleanup.sh
+++ centos7/scripts/cleanup.sh
@@ -3,8 +3,9 @@
 # Remove Ansible and its dependencies.
 yum -y remove ansible
 
-# Remove kickstart file
+# Remove kickstart files
 rm -f /root/original-ks.cfg
+rm -f /root/anaconda-ks.cfg
 
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M

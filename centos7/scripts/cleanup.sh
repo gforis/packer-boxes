d651f3bc079ad85b6ea6a19eb40add7c58f62d42
Gabor Foris <gabor_foris@epam.com>
2020-12-13 21:30:08 +0100

    removing /root/original-ks.cfg

--- centos7/scripts/cleanup.sh
+++ centos7/scripts/cleanup.sh
@@ -3,6 +3,9 @@
 # Remove Ansible and its dependencies.
 yum -y remove ansible
 
+# Remove kickstart file
+rm -f /root/original-ks.cfg
+
 # Zero out the rest of the free space using dd, then delete the written file.
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
